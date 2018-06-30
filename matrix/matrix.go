43588835c183a08fe863b018e5fe06d121f2e4ad
Nico Mitchell <namitc02@louisville.edu>
2018-06-30 00:36:18 -0400

    added basic functions

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,8 +1,38 @@
 package matrix
 
+import (
+	"fmt"
+)
+
 //Matrix represents an mxn matrix
 type Matrix struct {
 	m      int
 	n      int
 	values [][]float64
+}
+
+//New returns a new matrix
+func New(m, n int, values [][]float64) *Matrix {
+	return &Matrix{
+		m:      m,
+		n:      n,
+		values: values,
+	}
+}
+
+//At returns the value in matrix
+func (m *Matrix) At(row, col int) float64 {
+	return m.values[row][col]
+}
+
+func (m *Matrix) String() string {
+	var out string
+	for _, row := range m.values {
+		out += "|\t"
+		for _, val := range row {
+			out = fmt.Sprintf("%s %.3f", out, val)
+		}
+		out += " \t|\n"
+	}
+	return out
 }
