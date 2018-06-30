9d66295fff7f327bedde1ae75cd0c4bb039e0ddf
Nico Mitchell <namitc02@louisville.edu>
2018-06-30 02:23:56 -0400

    refactored and renamed echelon form functions

--- operations/echelon.go
+++ operations/echelon.go
@@ -17,48 +17,6 @@ func EchelonForm(m *matrix.Matrix) *matrix.Matrix {
 	return matrix.New(h, w, newRows)
 }
 
-func subtractRow(r1, r2 []float64) []float64 {
-	out := make([]float64, len(r1))
-	for i := range r1 {
-		out[i] = r1[i] - r2[i]
-		//just in case of floating point math fuckery
-		if out[i] < 0.00001 && out[i] > 0.00001 {
-			out[i] = 0
-		}
-	}
-	return out
-}
-
-func scaleRow(row []float64, factor float64) []float64 {
-	out := []float64{}
-	for _, v := range row {
-		out = append(out, v*factor)
-	}
-	return out
-}
-
-func findPivot(r []float64, width int) int {
-	for i := 0; i < width; i++ {
-		if r[i] != 0 {
-			return i
-		}
-	}
-	return -1
-}
-
-func swapRows(m [][]float64, r1, r2 int) [][]float64 {
-	temp := m[r1]
-	m[r1] = m[r2]
-	m[r2] = temp
-	return m
-}
-
-func doRowOpsEchelon(pivot int, r2, r []float64) []float64 {
-	factor := r2[pivot] / r[pivot]
-	scaled := scaleRow(r, factor)
-	return subtractRow(r2, scaled)
-}
-
 func echelonArithmetic(m [][]float64, h, w int) [][]float64 {
 	for i := 0; i < h; i++ {
 		r := m[i]
@@ -95,5 +53,47 @@ func echelonRowSwap(m [][]float64, h, w int) [][]float64 {
 			}
 		}
 	}
+	return m
+}
+
+func findPivot(r []float64, width int) int {
+	for i := 0; i < width; i++ {
+		if r[i] != 0 {
+			return i
+		}
+	}
+	return -1
+}
+
+func doRowOpsEchelon(pivot int, r2, r []float64) []float64 {
+	factor := r2[pivot] / r[pivot]
+	scaled := scaleRow(r, factor)
+	return subtractRow(r2, scaled)
+}
+
+func subtractRow(r1, r2 []float64) []float64 {
+	out := make([]float64, len(r1))
+	for i := range r1 {
+		out[i] = r1[i] - r2[i]
+		//just in case of floating point math fuckery
+		if out[i] < 0.00001 && out[i] > 0.00001 {
+			out[i] = 0
+		}
+	}
+	return out
+}
+
+func scaleRow(row []float64, factor float64) []float64 {
+	out := []float64{}
+	for _, v := range row {
+		out = append(out, v*factor)
+	}
+	return out
+}
+
+func swapRows(m [][]float64, r1, r2 int) [][]float64 {
+	temp := m[r1]
+	m[r1] = m[r2]
+	m[r2] = temp
 	return m
 }
